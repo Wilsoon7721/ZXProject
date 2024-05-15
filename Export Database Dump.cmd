f8526a4c838d262951ad733ecb89e12a2cffbe36
Wilsoon <wilsonoon2018@gmail.com>
2024-05-15 17:50:49 +0800

    Update Export Database Dump.cmd

--- Export Database Dump.cmd
+++ Export Database Dump.cmd
@@ -6,12 +6,15 @@ set DATABASE_NAME=simpleblog
 set MYSQL_USER=root
 set MYSQL_PASSWORD=root
 
+mysqldump -u %MYSQL_USER% -p%MYSQL_PASSWORD% --databases %DATABASE_NAME% > %DUMP_FILE%
+
+echo Database %DATABASE_NAME% has been exported to %DUMP_FILE%.
+
+timeout 3 /nobreak >nul
+echo Pushing to Git repository...
 cd %USERPROFILE%\Desktop\ZXProject
 git add -A
 git commit -m "Code Modifications"
 git push
 
-mysqldump -u %MYSQL_USER% -p%MYSQL_PASSWORD% --databases %DATABASE_NAME% > %DUMP_FILE%
-
-echo Database %DATABASE_NAME% has been exported to %DUMP_FILE%.
 timeout 5 /nobreak >nul
